df4ee5ff4ace43bc0c4930c47c3c7ef283fe9e23
spiralgrisou <spiralgrisou@outlook.com>
2020-11-16 00:53:28 +0200

    Create Homing client for connections

---
+++ HomingClient/JoinForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace HomingClient
+{
+    partial class JoinForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JoinForm));
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.creditLink = new System.Windows.Forms.LinkLabel();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.joinBtn = new System.Windows.Forms.Button();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(97, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(404, 65);
+            this.label4.TabIndex = 7;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 189);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // creditLink
+            // 
+            this.creditLink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.creditLink.AutoSize = true;
+            this.creditLink.Location = new System.Drawing.Point(462, 255);
+            this.creditLink.Name = "creditLink";
+            this.creditLink.Size = new System.Drawing.Size(103, 13);
+            this.creditLink.TabIndex = 6;
+            this.creditLink.TabStop = true;
+            this.creditLink.Text = "Made by spiralgrisou";
+            this.creditLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.creditLink_LinkClicked);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitBtn.Location = new System.Drawing.Point(462, 134);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(93, 30);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // joinBtn
+            // 
+            this.joinBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.joinBtn.Location = new System.Drawing.Point(363, 134);
+            this.joinBtn.Name = "joinBtn";
+            this.joinBtn.Size = new System.Drawing.Size(93, 30);
+            this.joinBtn.TabIndex = 1;
+            this.joinBtn.Text = "Join";
+            this.joinBtn.UseVisualStyleBackColor = true;
+            this.joinBtn.Click += new System.EventHandler(this.joinBtn_Click);
+            // 
+            // portBox
+            // 
+            this.portBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.portBox.Location = new System.Drawing.Point(14, 94);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(545, 20);
+            this.portBox.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.portLabel);
+            this.panel1.Controls.Add(this.addressLabel);
+            this.panel1.Controls.Add(this.exitBtn);
+            this.panel1.Controls.Add(this.joinBtn);
+            this.panel1.Controls.Add(this.portBox);
+            this.panel1.Controls.Add(this.addressBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 183);
+            this.panel1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Do you want to join a Homing server?";
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(14, 78);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(97, 13);
+            this.portLabel.TabIndex = 3;
+            this.portLabel.Text = "<default port label>";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(14, 31);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(110, 13);
+            this.addressLabel.TabIndex = 3;
+            this.addressLabel.Text = "<default adress label>";
+            // 
+            // addressBox
+            // 
+            this.addressBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addressBox.Location = new System.Drawing.Point(14, 47);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(545, 20);
+            this.addressBox.TabIndex = 2;
+            // 
+            // JoinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 277);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.creditLink);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(736, 317);
+            this.MinimumSize = new System.Drawing.Size(593, 316);
+            this.Name = "JoinForm";
+            this.Text = "JoinForm";
+            this.Load += new System.EventHandler(this.JoinForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel creditLink;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button joinBtn;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.TextBox addressBox;
+    }
+}
---
+++ HomingClient/JoinForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using NetworkingManager;
+
+namespace HomingClient
+{
+    public partial class JoinForm : Form
+    {
+        public JoinForm()
+        {
+            InitializeComponent();
+        }
+
+        private void JoinForm_Load(object sender, EventArgs e)
+        {
+            string currentAddress = NetworkingInformation.GetLocalIPAddress();
+            if (String.IsNullOrEmpty(currentAddress))
+            {
+                MessageBox.Show("No network connection was found, Please connect to the internet and try again.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            addressLabel.Text = "Enter an address here: (Default address used: " + NetworkingInformation.GetLocalIPAddress() + ")";
+            portLabel.Text = "Enter a port to host on here: (Default port used: 1337)";
+        }
+
+        private void creditLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            e.Link.Visited = true;
+            Process.Start("https://www.github.com/spiralgrisou");
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void joinBtn_Click(object sender, EventArgs e)
+        {
+            string defaultAddress = NetworkingInformation.GetLocalIPAddress();
+
+            int defaultPort = 1337;
+            bool useDefaultAddr = false;
+            bool useDefaultPort = false;
+            if (String.IsNullOrEmpty(addressBox.Text))
+                useDefaultAddr = true;
+            if (String.IsNullOrEmpty(portBox.Text))
+                useDefaultPort = true;
+            ServerForm serverForm = new ServerForm();
+            if (useDefaultAddr)
+                serverForm.IP_ADDRESS = defaultAddress;
+            else
+            {
+                if (NetworkingInformation.IsValidIPAddress(addressBox.Text))
+                    serverForm.IP_ADDRESS = addressBox.Text;
+                else
+                {
+                    MessageBox.Show("Please enter a valid IP Address, or leave it empty to use the default address.", "Argument Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            if (useDefaultPort)
+                serverForm.PORT = defaultPort;
+            else
+            {
+                try
+                {
+                    int PORT = Int32.Parse(portBox.Text);
+                    if (PORT < 0)
+                    {
+                        MessageBox.Show("Ports need to be greater than 0.", "Argument Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    serverForm.PORT = PORT;
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Please enter a valid port, or leave the port box empty to use the default port.", "Argument Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            Hide();
+            serverForm.ShowDialog();
+            Close();
+        }
+    }
+}
