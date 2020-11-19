a9a7410bf4a255a94b8911821567573c1c57cc7c
spiralgrisou <spiralgrisou@outlook.com>
2020-11-20 00:27:26 +0200

    Add net data

---
+++ NetworkingManager/NetData.cs
@@ -0,0 +1,23 @@
+﻿namespace NetworkingManager
+{
+    public class NetData
+    {
+        #region Shared data
+        public enum LoggingMethod
+        {
+            Console,
+            Forms
+        }
+        #endregion
+        #region NetServer Delegates
+        public delegate void MessageDispatcher(NetConnection connection, string msg);
+        public delegate void ConnectionSuccess(NetConnection connection);
+        public delegate void ConnectionDisconnection(NetConnection connection);
+        #endregion
+        #region NetClient Delegates
+        public delegate void MessageReceived(string msg);
+        public delegate void Disconnected();
+        public delegate void Connected();
+        #endregion
+    }
+}
---
+++ NetworkingManager/NetParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkingManager
+{
+    public class NetMessage
+    {
+        private string _command { get; set; }
+        private List<string> _args = new List<string>();
+
+        public NetMessage(string message)
+        {
+            try
+            {
+                string[] parts = message.Split('/');
+                string command = parts[0] != null ? parts[0] : String.Empty;
+                List<string> args = new List<string>();
+                foreach (string part in parts)
+                {
+                    if (part == parts[0])
+                        return;
+                    _args.Add(part);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Something went wrong
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public string GetCommand()
+        {
+            return _command;
+        }
+
+        public bool HasCommand()
+        {
+            return !String.IsNullOrEmpty(_command) ? true : false;
+        }
+
+        public List<string> GetArguments()
+        {
+            return _args;
+        }
+    }
+
+    public class NetParser
+    {
+        public static NetMessage ParseMessage(string msg)
+        {
+            return new NetMessage(msg);
+        }
+    }
+}
